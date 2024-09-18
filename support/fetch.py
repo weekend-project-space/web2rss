6eb92247a811bde7e25ae02b7206c68e97dfdd33
weekend-project-space <sunxzg@gmail.com>
2024-09-18 10:07:29 +0800

    fix: 不常用编码按utf8处理

--- support/fetch.py
+++ support/fetch.py
@@ -4,8 +4,14 @@ from support.config import get_item
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/\
-    537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36'
+    537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36',
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,\
+    image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;\
+    v=b3;q=0.7",
+    "Connection": "keep-alive"
 }
+
+_charsets = ['GB', 'UTF', 'ISO']
 
 
 def fetch(url, type='soup'):
@@ -14,9 +20,14 @@ def fetch(url, type='soup'):
         # print(f'fetch url: {url}')
         response = requests.get(url, headers=headers, timeout=10000)
         if response.status_code > 300:
-            raise RuntimeError(f'status err : {response.status_code}')
-        if response.apparent_encoding:
+            raise RuntimeError(f'status err : {response.status_code},\
+                                err: {response.content}')
+        print(response.apparent_encoding)
+        ec = response.apparent_encoding
+        if ec and len(list(filter(lambda c: c in ec, _charsets))):
             response.encoding = response.apparent_encoding
+        else:
+            response.encoding = 'utf-8'
         if type == 'soup':
             return BeautifulSoup(response.text, 'html.parser')
         elif type == 'text':
